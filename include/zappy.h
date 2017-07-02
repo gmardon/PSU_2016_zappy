16c651c59d0cc9f83c3afdd4d136f3783c87a9fe
aurelien <aolibe@gmail.com>
2017-07-02 23:45:16 +0200

    nooooooorme

--- include/zappy.h
+++ include/zappy.h
@@ -1,27 +1,27 @@
 #ifndef _ZAPPY_H_
-#define _ZAPPY_H_ 
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <stdarg.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <grp.h>
-#include <pwd.h>
-#include <time.h>
-#include <fcntl.h> /* Added for the nonblocking socket */
-#include <dirent.h>
-#define _GNU_SOURCE /* See feature_test_macros(7) */
-#include <stdio.h>
-#define TRUE 1
-#define FALSE 0
-#define __UNUSED__ __attribute__((unused))
-#define BUFFER_SIZE 2048
+# define _ZAPPY_H_
+# include <stdlib.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdarg.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <string.h>
+# include <grp.h>
+# include <pwd.h>
+# include <time.h>
+# include <fcntl.h>
+# include <dirent.h>
+# define _GNU_SOURCE
+# include <stdio.h>
+# define TRUE 1
+# define FALSE 0
+# define __UNUSED__ __attribute__((unused))
+# define BUFFER_SIZE 2048
 
 void my_error(char *msg, int exit_code);
 char *get_next_line(const int fd);
